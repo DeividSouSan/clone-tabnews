209664de031a9af0d7525a446b776177cf4d1607
Deivid Souza Santana <49818020+DeividSouSan@users.noreply.github.com>
2024-11-17 13:35:09 +0000

    adiciona os scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,32 @@
 function Home() {
-    return <p style={{ display: "flex", width: "400px", margin: "auto", textAlign: "justify" }}>
-        Cidadania em uma república: Não é o crítico que importa; nem aquele que aponta onde
-        foi que o homem tropeçou ou como o autor das façanhas
-        poderia ter feito melhor.
-        O crédito pertence ao homem que está por inteiro na
-        arena da vida, cujo rosto está manchado de poeira, suor e
-        sangue; que luta bravamente; que erra, que decepciona,
-        porque não há esforço sem erros e decepções; mas que, na
-        verdade, se empenha em seus feitos; que conhece o
-        entusiasmo, as grandes paixões; que se entrega a uma
-        causa digna; que, na melhor das hipóteses, conhece no
-        final o triunfo da grande conquista e que, na pior, se
-        fracassar, ao menos fracassa ousando grandemente.
+  return (
+    <p
+      style={{
+        display: "flex",
+        width: "400px",
+        margin: "auto",
+        textAlign: "justify",
+      }}
+    >
+      Cidadania em uma república: Não é o crítico que importa; nem aquele que
+      aponta onde foi que o homem tropeçou ou como o autor das façanhas poderia
+      ter feito melhor. O crédito pertence ao homem que está por inteiro na
+      arena da vida, cujo rosto está manchado de poeira, suor e sangue; que luta
+      bravamente; que erra, que decepciona, porque não há esforço sem erros e
+      decepções; mas que, na verdade, se empenha em seus feitos; que conhece o
+      entusiasmo, as grandes paixões; que se entrega a uma causa digna; que, na
+      melhor das hipóteses, conhece no final o triunfo da grande conquista e
+      que, na pior, se fracassar, ao menos fracassa ousando grandemente.
     </p>
+  );
 }
 
 export default Home;
+
+function teste() {
+  return "o";
+}
+
+function teste2() {
+  return "teste";
+}
