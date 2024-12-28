2c3323233215bab38007ae66cb635d40f343f973
Deivid Souza Santana <deividsantana2013@gmail.com>
2024-12-28 17:35:37 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,13 @@
-function status(request, response) {
-    response.status(200).json(
-        {
-            chave: "valor"
-        }
-    )
+import database from "../../../../infra/database.js"
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 AS SOMA;");
+  console.log(result.rows)
+  response.status(200).json(
+    {
+      chave: result
+    }
+  )
 }
 
 export default status;
