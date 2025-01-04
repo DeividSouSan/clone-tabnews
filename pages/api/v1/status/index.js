d75761c7744d141b1af5ee061de61fe54672b634
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-01-04 15:03:30 -0300

    finishes `api/v1/status`endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,13 +1,30 @@
 import database from "/infra/database.js"
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS SOMA;");
-  console.log(result.rows)
-  response.status(200).json(
-    {
-      chave: result
+
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;")
+    .then(query => query.rows[0].server_version)
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
+    .then(query => query.rows[0].max_connections)
+
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int as active_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB]
+  }).then(query => query.rows[0].active_connections)
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionResult,
+        max_connections: parseInt(databaseMaxConnectionsResult),
+        opened_connections: databaseOpenedConnectionsResult
+      }
     }
-  )
+  });
 }
 
 export default status;
