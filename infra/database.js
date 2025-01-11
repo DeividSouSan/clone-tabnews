667b0b96f5b698d5f8417e6dd10ecfdca72a66d4
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-01-11 14:58:05 -0300

    database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -6,16 +6,9 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB
+    database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === 'development' ? false : true,
   });
-
-  console.log("Credenciais do postgres: ", {
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB
-  })
 
   try {
     await client.connect();
