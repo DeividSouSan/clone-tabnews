72f40efaad0f1303754ef09f9d58682800802128
Deivid Souza Santana <deividsantana2013@gmail.com>
2025-07-02 05:50:53 -0300

    test: rename tests description to match new pattern

--- tests/integration/api/v1/migrations/delete.test.js
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -8,23 +8,27 @@ beforeAll(() => {
   return cleanDatabase();
 });
 
-test("PUT to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+describe("DELETE /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Forbid user from deleting data", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+
+      expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+      expect(responseBody.error).toBe("Method DELETE Not Allowed.");
+    });
+
+    afterEach(async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+      if (responseBody.dependencies.database.opened_connections !== 1) {
+        throw new Error(
+          "Conexões que foram abertas não foram fechadas adequadamente.",
+        );
+      }
+    });
   });
-
-  expect(response.status).toBe(405);
-
-  const responseBody = await response.json();
-  expect(responseBody.error).toBe("Method DELETE Not Allowed.");
-});
-
-afterEach(async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  const responseBody = await response.json();
-  if (responseBody.dependencies.database.opened_connections !== 1) {
-    throw new Error(
-      "Conexões que foram abertas não foram fechadas adequadamente.",
-    );
-  }
 });
