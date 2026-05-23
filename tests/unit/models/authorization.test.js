ac3da7f1aa79dd05bf1cba4a1e4ea89906d48431
Deivid Souza Santana <deividsantana2013@gmail.com>
2026-05-23 17:50:42 -0300

    test: fix ISO date format in `authorization` unit tests

--- tests/unit/models/authorization.test.js
+++ tests/unit/models/authorization.test.js
@@ -82,8 +82,8 @@ describe("models/authorization.js", () => {
         email: "resource@curso.dev",
         password: "validpassword",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00:000Z",
-        updated_at: "2026-0101T00:00:00:000Z"
+        created_at: "2026-01-01T00:00:00:000Z",
+        updated_at: "2026-01-01T00:00:00:000Z"
       };
 
       let result = authorization.filterOutput(
@@ -96,8 +96,8 @@ describe("models/authorization.js", () => {
         id: 1,
         username: "resource",
         features: ["read:user"],
-        created_at: "2026-0101T00:00:00:000Z",
-        updated_at: "2026-0101T00:00:00:000Z"
+        created_at: "2026-01-01T00:00:00:000Z",
+        updated_at: "2026-01-01T00:00:00:000Z"
       });
     });
   });
