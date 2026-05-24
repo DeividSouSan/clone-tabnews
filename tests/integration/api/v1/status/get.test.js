b9542dc7c2ffc362000b0c76ec578f26b34fd299
Deivid Souza Santana <deividsantana2013@gmail.com>
2026-05-24 17:34:57 -0300

    test: add default user coverage for `GET /api/v1/status` tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -24,6 +24,29 @@ describe("GET to /api/v1/status", () => {
     });
   });
 
+  describe("Authenticated user", () => {
+    test("Retrieving current system status", async () => {
+      const user = await orchestrator.createUserWithSession();
+
+      const response = await fetch(`${webserver.origin}/api/v1/status`, {
+        headers: {
+          Cookie: `session_id=${user.token}`
+        }
+      });
+
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+      expect(responseBody.updated_at).toBeDefined();
+
+      const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+      expect(responseBody.dependencies.database).not.toHaveProperty("version");
+    });
+  });
+
   describe("Privileged user", () => {
     test("Retrieving current system status", async () => {
       const user = await orchestrator.createUserWithSession();
