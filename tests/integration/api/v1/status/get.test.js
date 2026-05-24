33bd1ee2069dd11da7b16a20a42816f4698d317d
Deivid Souza Santana <deividsantana2013@gmail.com>
2026-05-24 17:31:00 -0300

    test: clear database and run migrations in `GET /api/v1/status` tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,6 +3,8 @@ import orchestrator from "tests/orchestrator";
 import webserver from "infra/webserver";
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
 });
 
 describe("GET to /api/v1/status", () => {
